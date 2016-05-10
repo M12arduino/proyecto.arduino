b85b6bead37cc7b96f7b2b29be7359fd01757d14
jordipuig2 <jordipuig91@gmail.com>
2016-05-10 17:47:40 +0200

    Versió 0.2 del projecte prjava01

---
+++ src/java/m12/arduino/domain/Robot.java
@@ -0,0 +1,18 @@
+package m12.arduino.domain;
+/*
+Jordi Puig Puig
+DAW 2
+Curs 2015-2016
+codi, nom, ubicació (lloc i/o coordenades), estat (lliure, ocupat,
+desconnectat, local)
+@author Jordi
+*/
+public class Robot {
+
+    // ATTR
+    private long id_robot;
+    private String nombre;
+    private Ubicacion ubicacion;
+    private EstadoRobot estado;
+    
+}
---
+++ src/java/m12/arduino/domain/Ubicacion.java
@@ -0,0 +1,16 @@
+package m12.arduino.domain;
+/*
+Jordi Puig Puig
+DAW 2
+Curs 2015-2016
+
+@author Jordi
+*/
+class Ubicacion {
+
+    // ATTR
+    private String lugar;
+    private float coorX;
+    private float coorY;
+    
+}
