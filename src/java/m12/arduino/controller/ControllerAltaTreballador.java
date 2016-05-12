7230ec8cfe859058af195372616c635c19072c1a
Pablo Couto <pablocouto3@gmail.com>
2016-05-12 20:41:55 +0200

    Versió 0.2 del projecte prjava01

--- src/java/m12/arduino/controller/ControllerAltaTreballador.java
+++ src/java/m12/arduino/controller/ControllerAltaTreballador.java
@@ -1,7 +1,5 @@
 package m12.arduino.controller;
 
-import org.springframework.stereotype.Controller;
-
 /*
 Jordi Puig Puig
 DAW 2
@@ -9,7 +7,7 @@ Curs 2015-2016
 
 @author Jordi
 */
-@Controller
+
 public class ControllerAltaTreballador {
 
 }
