4df2badc6ee174926f4d3eb6a856fd83dc6d6974
jordipuig2 <jordipuig91@gmail.com>
2016-05-12 16:37:02 +0200

    estructura de classes v2

---
+++ src/java/m12/arduino/controller/ControllerAltaTreballador.java
@@ -0,0 +1,15 @@
+package m12.arduino.controller;
+
+import org.springframework.stereotype.Controller;
+
+/*
+Jordi Puig Puig
+DAW 2
+Curs 2015-2016
+
+@author Jordi
+*/
+@Controller
+public class ControllerAltaTreballador {
+
+}
