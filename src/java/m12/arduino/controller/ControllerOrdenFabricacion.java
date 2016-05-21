5f2858aa754cd50cde4ed94c5d10d491740ae8b7
mcninu23 <mcninu23@hotmail.com>
2016-05-21 21:44:57 +0200

    Versió 0.3 del projecte prjava01

--- src/java/m12/arduino/controller/ControllerOrdenFabricacion.java
+++ src/java/m12/arduino/controller/ControllerOrdenFabricacion.java
@@ -1,13 +1,21 @@
 package m12.arduino.controller;
 
+import java.io.IOException;
+import java.util.List;
+import m12.arduino.domain.CategoriaTrabajador;
 import m12.arduino.domain.OrdenFabricacion;
 import m12.arduino.domain.Prioridad;
+import m12.arduino.domain.Trabajador;
 import m12.arduino.service.OrdenFabricacionForm;
 import m12.arduino.service.ServiceOrdenFabricacion;
 import m12.arduino.service.ServiceProceso;
 import m12.arduino.service.ServiceRobot;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 /*
@@ -52,5 +60,71 @@ public class ControllerOrdenFabricacion {
         ModelAndView mV = new ModelAndView("ordenFabricacionDetalle");        
         mV.addObject("ordenFabricacion", orden);
         return mV;
+    }
+    
+    @RequestMapping(value="/actualizar",headers = {"Content-type=application/json"}, method = RequestMethod.POST)
+    public @ResponseBody String actualizarOrdenFabricacion(@RequestBody OrdenFabricacionForm ofF){
+        String msg = "";
+        try {
+            OrdenFabricacion of = new OrdenFabricacion();
+            of.setId(ofF.getId());
+            of.setCodigo(ofF.getCodigo());
+            of.setDescripcion(ofF.getDescripcion());
+            of.setCantidad(ofF.getCantidad());
+            of.setProceso(sP.buscarProceso(ofF.getCodigo_proceso()));
+            of.setProridad(ofF.getPrioridad());
+            of.setRobot(sR.buscarRobot(ofF.getId_robot()));
+            sO.actualizarOrden(of);
+            msg = "Fabrication Order updated";
+        } catch (Exception e) {
+            msg = "updated fail "+e.getMessage();
+        }
+        return msg;
+    }
+    
+    @RequestMapping(value="/eliminar",headers = {"Content-type=application/json"}, method = RequestMethod.POST)
+    public @ResponseBody String eliminarOrdenFabricacion(@RequestBody OrdenFabricacionForm ofF){
+        String msg = "";
+        try {
+            OrdenFabricacion of = new OrdenFabricacion();
+            of.setId(ofF.getId());
+            sO.eliminarOrden(of);
+            msg = "Fabrication Order deleted";
+        } catch (Exception e) {
+            msg = "delete fail "+e.getMessage();
+        }
+        return msg;
+    }
+    
+    @RequestMapping(value="/administrar")
+    public ModelAndView administrarOrdenFabricacion(){
+        ModelAndView mV = new ModelAndView("ordenFabricacionCrud","command",new OrdenFabricacionForm());
+        mV.addObject("prioridades", Prioridad.values());
+        mV.addObject("procesos", sP.listarProcesos());
+        mV.addObject("robots", sR.listarRobots());
+        return mV;
+    }
+    
+    @RequestMapping(value = "/buscar",headers = {"Content-type=application/json"}, method = RequestMethod.POST)
+    public @ResponseBody String buscaOrdenFabricacionAjax(@RequestBody OrdenFabricacionForm ofF) {
+        String codigo = ofF.getCodigo();
+        String descripcion = ofF.getDescripcion();
+        Prioridad prioridad = ofF.getPrioridad();
+        if (prioridad == Prioridad.INDEFINIDO) prioridad = null;
+        String response = "";
+        //List<OrdenFabricacion> OF = sO.listarOrdenes("codigo",codigo,"descripcion",descripcion);
+        List<OrdenFabricacion> OF = sO.listarOrdenes();
+        if(OF !=null){
+        try {
+             ObjectMapper mapperObj = new ObjectMapper();
+             response = mapperObj.writeValueAsString(OF);
+        } catch (IOException ex) {
+            response = ex.getMessage();
+        } 
+        }else{
+            response = null;
+        }
+
+        return response;
     }
 }
